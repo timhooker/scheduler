6f6dc98564d2f373cb8865cf57ffccdb7c60cb97
Tim Hooker <timhooker@me.com>
2015-02-02 22:46:35 -0500

    completed delete modal and basic wiring of nav buttons

--- src/js/nav_buttons.js
+++ src/js/nav_buttons.js
@@ -1,25 +1,31 @@
 function navButtons() {
   $('.appt-nav').on('click', '.add-btn', function() {
     showSection('#appt-edit');
+    // $('#appt-edit').removeClass('inactive');
     navButtons();
+
   });
 
   $('.appt-nav').on('click', '.back-btn', function() {
     showSection('#appt-listing');
     navButtons();
     listingsPageEvents();
+    // $('.page').removeClass('inactive');
+
   });
   $('.appt-nav').on('click', '.edit-btn', function() {
     showSection('#appt-edit');
     navButtons();
+    // $('.page').removeClass('inactive');
+
   });
 }
 
 function showSection(sectionID) {
   var section = $(sectionID).html();
-
   $('.wrapper').html(
-    '<div id="' + sectionID + '">' +
+    '<div id="' + sectionID + '" class="page">' +
     section +
     '</div>');
+
 }
