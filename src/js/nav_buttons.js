a9cad01fcf1bd8ebcc97bf8b8631fe50981955ae
Tim Hooker <timhooker@me.com>
2015-02-03 10:09:37 -0500

    cleaned up files

--- src/js/nav_buttons.js
+++ src/js/nav_buttons.js
@@ -1,9 +1,9 @@
 function navButtons() {
+
   $('.appt-nav').on('click', '.add-btn', function() {
     showSection('#appt-edit');
     // $('#appt-edit').removeClass('inactive');
     navButtons();
-
   });
 
   $('.appt-nav').on('click', '.back-btn', function() {
@@ -11,21 +11,20 @@ function navButtons() {
     navButtons();
     listingsPageEvents();
     // $('.page').removeClass('inactive');
-
   });
+
   $('.appt-nav').on('click', '.edit-btn', function() {
     showSection('#appt-edit');
     navButtons();
     // $('.page').removeClass('inactive');
-
   });
 }
 
 function showSection(sectionID) {
   var section = $(sectionID).html();
+
   $('.wrapper').html(
     '<div id="' + sectionID + '" class="page">' +
     section +
     '</div>');
-
 }
