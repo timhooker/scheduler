0379b8cfda47931ef6c3424510a50b8b8120e232
Tim Hooker <timhooker@me.com>
2015-02-03 15:58:24 -0500

    add apt id

--- src/js/appointment.js
+++ src/js/appointment.js
@@ -2,12 +2,13 @@
 //appt
 app.Appointment = function (spec) {
   //TODO Add Constructor Function
+
   return {
     title: spec.title,
     date: spec.date,
     time: spec.time,
     address: spec.address,
-    aptId: 0,
+    aptId: Date.now(),
     equal: function (otherUser) {
       return self.aptId === otherUser.aptId;
     }
