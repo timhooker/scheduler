a7850a826fbc9858f66682abf93460184f9f93ae
Bernard <worthybernard@gmail.com>
2015-02-04 05:49:31 -0500

    Started using app.views in scheduler.js

--- src/js/scheduler.js
+++ src/js/scheduler.js
@@ -1,12 +1,7 @@
 $(function() {
 
-  // var apptListing = $('#appt-listing').html();
-
   $('.wrapper').html(
     app.views['appt-listing']);
-    // '<div id="appt-listing" class="page">' +
-    // apptListing +
-    // '</div>');
 
   navButtons();
   listingsPageEvents();
