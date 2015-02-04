b85401ade2f4711c0f2600ed7c6f46fb94ec62e0
Tim Hooker <timhooker@me.com>
2015-02-03 23:29:41 -0500

    working on page manager object and was able to implement initial changes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,36 @@
 $(function() {
 
   // display appointments we have currently
-  app.listAppointments();
+  app.manager = app.PageManager();
+  app.manager.registerPage('listings', function() {
+    var pageID = 'appt-listing';
+
+    $('.wrapper').html(
+      app.views['appt-listing']({ appointments: appointments, pageID: pageID }) );
+
+    navButtons();
+    listingsPageEvents();
+  });
+  app.manager.registerPage('view', function() {
+    var pageID = 'appt-view';
+
+    $('.wrapper').html(
+      app.views['appt-view']({ appointment: appointments[0], pageID: pageID }) );
+
+    navButtons();
+  });
+  app.manager.registerPage('edit', function() {
+    var pageID = 'appt-edit';
+
+    $('.wrapper').html(
+      app.views['appt-edit']({ appointment: appointments[0], pageID: pageID }) );
+
+    navButtons();
+  });
+
+  app.aptManager = app.AppointmentStore();
+  var appointments = app.aptManager.query();
+  app.manager.goTo('listings', appointments);
+
 
 });
