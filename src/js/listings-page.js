f1d8398b4a81c80f193afc9d8571fd5e26ce7b66
Bernard <worthybernard@gmail.com>
2015-02-04 16:22:39 -0500

    Added Delete from store function

--- src/js/listings-page.js
+++ src/js/listings-page.js
@@ -10,12 +10,16 @@ function listingsPageEvents() {
     // clicking on delete brings up the delete prompt
     var deleteBtn = $(this);
     var appt = deleteBtn.closest('.appt');
+    var aptId = appt.data("id");
     var confirmDelete = appt.children('.appt-delete');
     // show the delete
     confirmDelete.fadeIn('fast');
     // confirm delete to remove
     confirmDelete.children('.appt-confirm-delete').on('click', function() {
       appt.remove();
+      var index = app.aptManager.findById(aptId);
+      console.log(index);
+      app.aptManager.remove(index);
     });
     // if cancelled, remove
     confirmDelete.children('.appt-cancel-delete').on('click', function() {
