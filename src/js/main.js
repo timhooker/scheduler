22add724296451eb1037ad8b3ade364537e2b661
Bernard <worthybernard@gmail.com>
2015-02-09 08:07:03 -0500

    Fixed sort again

--- src/js/main.js
+++ src/js/main.js
@@ -6,17 +6,18 @@ $(function() {
       var pageID = 'appt-listing';
       var listingArr = app.aptManager.query();
 
+      listingArr.sort(function(a,b) {
+        a = (a.time);
+        b = (b.time);
+        return a<b ? -1 : a>b ? 1 : 0;
+      });
+      
       listingArr.sort(function(a, b) {
         a = new Date(a.date);
         b = new Date(b.date);
         return a<b ? -1 : a>b ? 1 : 0;
       });
 
-      listingArr.sort(function(a,b) {
-        a = (a.time);
-        b = (b.time);
-        return a<b ? -1 : a>b ? 1 : 0;
-      });
 
       $('.wrapper').append(
         app.views['appt-listing']({ appointments: app.aptManager.query(), pageID: pageID }) );
